85ce750e97d277cc763c5b3800944905c085987a
Nikola Cvitaš <nikola.cvitas@btb.hr>
2019-12-31 11:28:33 +0100

    Added css and html

--- ppmtool-react-client/src/App.js
+++ ppmtool-react-client/src/App.js
@@ -2,6 +2,7 @@ import React from "react";
 import "./App.css";
 import Dashborad from "./compoments/Dashborad";
 import Header from "./compoments/Layout/Header";
+import "bootstrap/dist/css/bootstrap.min.css";
 
 function App() {
   return (
