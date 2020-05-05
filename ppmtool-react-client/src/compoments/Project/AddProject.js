af03de155ab184659af9521030b5d1e1a2f65077
Nikola Cvitaš <nikola.cvitas@btb.hr>
2020-05-05 15:22:28 +0200

    text removed

--- ppmtool-react-client/src/compoments/Project/AddProject.js
+++ ppmtool-react-client/src/compoments/Project/AddProject.js
@@ -38,8 +38,6 @@ class AddProject extends Component {
   render() {
     return (
       <div>
-        <h1>Add Project Form</h1>
-
         <div className="project">
           <div className="container">
             <div className="row">
