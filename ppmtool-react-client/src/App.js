364439caddba1f1e927203b8147898c7de161d39
Nikola Cvitaš <nikola.cvitas@btb.hr>
2020-05-05 10:38:39 +0200

    Added routes for Dashborad and addProject

--- ppmtool-react-client/src/App.js
+++ ppmtool-react-client/src/App.js
@@ -4,13 +4,15 @@ import Dashboard from "./compoments/Dashboard";
 import Header from "./compoments/Layout/Header";
 import "bootstrap/dist/css/bootstrap.min.css";
 import { BrowserRouter as Router, Route } from "react-router-dom";
+import AddProject from "./compoments/Project/AddProject";
 
 function App() {
   return (
     <Router>
       <div className="App">
         <Header />
-        <Dashboard />
+        <Route exact path="/dashboard" component={Dashboard} />
+        <Route exact path="/addProject" component={AddProject} />
       </div>
     </Router>
   );
