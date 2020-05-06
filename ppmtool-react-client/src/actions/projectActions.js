accbe19c8253919885c9fb83c082a52035def4a4
Nikola Cvitaš <nikola.cvitas@btb.hr>
2020-05-07 01:25:03 +0200

    Get Project by id, update case part 1

--- ppmtool-react-client/src/actions/projectActions.js
+++ ppmtool-react-client/src/actions/projectActions.js
@@ -20,3 +20,11 @@ export const getProjects = () => async (dispatch) => {
     payload: res.data,
   });
 };
+
+export const getProject = (id, history) => async (dispatch) => {
+  const res = await axios.get(`http://localhost:8080/api/project/${id}`);
+  dispatch({
+    type: GET_PROJECT,
+    payload: res.data,
+  });
+};
