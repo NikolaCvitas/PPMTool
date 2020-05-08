f222706321144c13ff972f961122908972e01b08
Nikola Cvitaš <nikola.cvitas@btb.hr>
2020-05-08 15:09:04 +0200

    Bug fix- delete operation, improved backlog/project task relation

--- ppmtool/src/main/java/io/agileintelligence/ppmtool/services/ProjectTaskService.java
+++ ppmtool/src/main/java/io/agileintelligence/ppmtool/services/ProjectTaskService.java
@@ -10,8 +10,6 @@ import io.agileintelligence.ppmtool.repositories.ProjectTaskRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import java.util.List;
-
 @Service
 public class ProjectTaskService {
 
@@ -107,11 +105,6 @@ public class ProjectTaskService {
 
     public void deletePTByProjectSequence(String backlog_id, String pt_id){
         ProjectTask projectTask = findPTByProjectSequence(backlog_id, pt_id);
-
-        Backlog backlog = projectTask.getBacklog();
-        List<ProjectTask> pts = backlog.getProjectTasks();
-        pts.remove(projectTask);
-        backlogRepository.save(backlog);
 
         projectTaskRepository.delete(projectTask);
     }
