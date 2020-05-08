da5515eb4b56eb6ede90b0fe8c9a79c617adddcf
Nikola Cvitaš <nikola.cvitas@btb.hr>
2020-05-08 14:57:06 +0200

    Finish up update validation and delete

--- ppmtool/src/main/java/io/agileintelligence/ppmtool/services/ProjectTaskService.java
+++ ppmtool/src/main/java/io/agileintelligence/ppmtool/services/ProjectTaskService.java
@@ -10,6 +10,8 @@ import io.agileintelligence.ppmtool.repositories.ProjectTaskRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+
 @Service
 public class ProjectTaskService {
 
@@ -95,11 +97,22 @@ public class ProjectTaskService {
     public ProjectTask updateByProjectSequence(ProjectTask updatedTask, String backlog_id, String pt_id){
 
         //find existing project task
-        ProjectTask projectTask = projectTaskRepository.findByProjectSequence(pt_id);
+        ProjectTask projectTask = findPTByProjectSequence(backlog_id, pt_id);
 
         //replace it with updated task
         projectTask = updatedTask;
 
         return projectTaskRepository.save(projectTask);
+    }
+
+    public void deletePTByProjectSequence(String backlog_id, String pt_id){
+        ProjectTask projectTask = findPTByProjectSequence(backlog_id, pt_id);
+
+        Backlog backlog = projectTask.getBacklog();
+        List<ProjectTask> pts = backlog.getProjectTasks();
+        pts.remove(projectTask);
+        backlogRepository.save(backlog);
+
+        projectTaskRepository.delete(projectTask);
     }
 }
