b8c16a76cef60e23ad62530d21f6b83aabca2887
Nikola Cvitaš <nikola.cvitas@btb.hr>
2020-05-12 15:23:16 +0200

    Lock operations to specific User(Read and Delete)

--- ppmtool/src/main/java/io/agileintelligence/ppmtool/repositories/ProjectRepository.java
+++ ppmtool/src/main/java/io/agileintelligence/ppmtool/repositories/ProjectRepository.java
@@ -11,4 +11,6 @@ public interface ProjectRepository extends CrudRepository<Project,Long> {
     Iterable<Project> findAllById(Iterable<Long> iterable);
 
     Project findByProjectIdentifier(String projectId);
+
+    Iterable<Project> findAllByProjectLeader(String name);
 }
