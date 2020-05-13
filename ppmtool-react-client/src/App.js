dd461bcda41caebc6e36f57e6b78de632071029a
Nikola Cvitaš <nikola.cvitas@btb.hr>
2020-05-13 15:37:16 +0200

    Setting Register, Login, Landing, refactoring wrong named components folder

--- ppmtool-react-client/src/App.js
+++ ppmtool-react-client/src/App.js
@@ -1,37 +1,57 @@
-import React from "react";
+import React, { Component } from "react";
 import "./App.css";
-import Dashboard from "./compoments/Dashboard";
-import Header from "./compoments/Layout/Header";
+import Dashboard from "./components/Dashboard";
+import Header from "./components/Layout/Header";
 import "bootstrap/dist/css/bootstrap.min.css";
 import { BrowserRouter as Router, Route } from "react-router-dom";
-import AddProject from "./compoments/Project/AddProject";
+import AddProject from "./components/Project/AddProject";
 import { Provider } from "react-redux";
 import store from "./store";
-import UpdateProject from "./compoments/Project/UpdateProject";
-import ProjectBoard from "./compoments/ProjectBoard/ProjectBoard";
-import AddProjectTask from "./compoments/ProjectBoard/ProjectTasks/AddProjectTask";
-import UpdateProjectTask from "./compoments/ProjectBoard/ProjectTasks/UpdateProjectTask";
+import UpdateProject from "./components/Project/UpdateProject";
+import ProjectBoard from "./components/ProjectBoard/ProjectBoard";
+import AddProjectTask from "./components/ProjectBoard/ProjectTasks/AddProjectTask";
+import UpdateProjectTask from "./components/ProjectBoard/ProjectTasks/UpdateProjectTask";
+import Landing from "./components/Layout/Landing";
+import Register from "./components/UserManagment/Register";
+import Login from "./components/UserManagment/Login";
 
-function App() {
-  return (
-    <Provider store={store}>
-      <Router>
-        <div className="App">
-          <Header />
-          <Route exact path="/dashboard" component={Dashboard} />
-          <Route exact path="/addProject" component={AddProject} />
-          <Route exact path="/updateProject/:id" component={UpdateProject} />
-          <Route exact path="/projectBoard/:id" component={ProjectBoard} />
-          <Route exact path="/addProjectTask/:id" component={AddProjectTask} />
-          <Route
-            exact
-            path="/updateProjectTask/:backlog_id/:pt_id"
-            component={UpdateProjectTask}
-          />
-        </div>
-      </Router>
-    </Provider>
-  );
+class App extends Component {
+  render() {
+    return (
+      <Provider store={store}>
+        <Router>
+          <div className="App">
+            <Header />
+            {
+              //Public Routes
+            }
+
+            <Route exact path="/" component={Landing} />
+            <Route exact path="/register" component={Register} />
+            <Route exact path="/login" component={Login} />
+
+            {
+              //Private Routes
+            }
+            <Route exact path="/dashboard" component={Dashboard} />
+            <Route exact path="/addProject" component={AddProject} />
+            <Route exact path="/updateProject/:id" component={UpdateProject} />
+            <Route exact path="/projectBoard/:id" component={ProjectBoard} />
+            <Route
+              exact
+              path="/addProjectTask/:id"
+              component={AddProjectTask}
+            />
+            <Route
+              exact
+              path="/updateProjectTask/:backlog_id/:pt_id"
+              component={UpdateProjectTask}
+            />
+          </div>
+        </Router>
+      </Provider>
+    );
+  }
 }
 
 export default App;
